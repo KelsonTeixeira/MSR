183e61b47811386472386430d6f8325b2dab6de4
kelsonteixeira <txkelson@gmail.com>
2025-03-17 20:16:04 -0300

    add database schema

--- api/app/database/connection.js
+++ api/app/database/connection.js
@@ -6,6 +6,7 @@ const connection = new sequelize('msr_db', 'admin', 'post_password', {
     host: process.env.HOST,
     dialect: process.env.DIALECT,
     timezone: process.env.TIMEZONE,
+    schema: "msr",
     dialectOptions: {
         multipleStatements: true
     }
