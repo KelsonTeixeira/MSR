d0ad0e51eb0a3fe07bbd0597267170fb16f2ea69
kelsonteixeira <txkelson@gmail.com>
2025-03-18 14:37:06 -0300

    adjust environment variables

--- api/app/database/connection.js
+++ api/app/database/connection.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const sequelize = require("sequelize");
 
-const connection = new sequelize('msr_db', 'admin', 'post_password', {
+const connection = new sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
-    dialect: process.env.DIALECT,
+    dialect: 'postgres',
     timezone: process.env.TIMEZONE,
-    schema: "msr",
+    schema: process.env.SCHEMA,
     dialectOptions: {
         multipleStatements: true
     }
